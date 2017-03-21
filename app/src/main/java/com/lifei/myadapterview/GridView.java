01252ac29cae207cab1e00a5fed7422387dfdb43
lidaosheng <843168906@qq.com>
2017-03-21 20:34:31 +0800

    Update GridView.java

--- app/src/main/java/com/lifei/myadapterview/GridView.java
+++ app/src/main/java/com/lifei/myadapterview/GridView.java
@@ -8,7 +8,7 @@ public class GridView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //000fafa你是
+        //000fafa你是生生世世
         setContentView(R.layout.activity_grid_view);
     }
 }
